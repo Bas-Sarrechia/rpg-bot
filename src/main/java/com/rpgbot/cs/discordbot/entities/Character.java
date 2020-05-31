160a602914b3ab66dcceb96b3d9f462710b07ee0
Bas Sarrechia <bassarrechia@gmail.com>
2020-06-01 01:49:46 +0200

    merged

--- src/main/java/com/rpgbot/cs/discordbot/entities/Character.java
+++ src/main/java/com/rpgbot/cs/discordbot/entities/Character.java
@@ -12,6 +12,9 @@ public class Character {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    private DiscordUser discordUser;
     private String characterName;
     private boolean isActive;
 }
