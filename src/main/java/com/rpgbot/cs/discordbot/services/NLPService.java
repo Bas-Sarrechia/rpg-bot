474ded26da3a6399a69ee6a12433aab7a25f6d47
Bas Sarrechia <bassarrechia@gmail.com>
2020-06-21 18:56:39 +0200

    changed to NLP for future development

--- src/main/java/com/rpgbot/cs/discordbot/services/NLPService.java
+++ src/main/java/com/rpgbot/cs/discordbot/services/NLPService.java
@@ -1,15 +1,8 @@
 package com.rpgbot.cs.discordbot.services;
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.util.CoreMap;
-import org.javacord.api.entity.user.User;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
 import java.util.Properties;
 
 @Service
@@ -24,24 +17,19 @@ public class NLPService {
         properties.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment");
         pipeline = new StanfordCoreNLP(properties);
     }
+        /*
+    @EventListener
+    public void listen(DefaultMessageEvent defaultMessageEvent) {
 
-    /*
-    @PostConstruct
-    public void listen() {
-        botService.getDiscordApi().addMessageCreateListener(messageCreateEvent -> {
-            boolean isBot = messageCreateEvent.getMessageAuthor().asUser().map(User::isBot).orElse(false);
-            if (!isBot) {
-                Annotation annotation = pipeline.process(messageCreateEvent.getMessageContent());
-                List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-                for (CoreMap sentence : sentences) {
-                    String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
-                    System.out.println(sentiment);
-                    if (!sentiment.equals("Neutral")) {
-                        messageCreateEvent.getChannel().sendMessage("Aww you sound " + sentiment);
-                    }
-                }
+        Annotation annotation = pipeline.process(defaultMessageEvent.getMessage());
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+            String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+            System.out.println(sentiment);
+            if (!sentiment.equals("Neutral")) {
+                defaultMessageEvent.getTarget().sendMessage("Aww you sound " + sentiment);
             }
-        });
+        }
     }
-     */
+             */
 }
