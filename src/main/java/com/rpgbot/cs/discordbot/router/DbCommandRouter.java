50bd5ffbb0a57e299ec5fc498ffab14216020e99
Sara Mae <34497797+saramaebee@users.noreply.github.com>
2020-06-23 20:56:15 -0400

    adding modifycommand

--- src/main/java/com/rpgbot/cs/discordbot/router/DbCommandRouter.java
+++ src/main/java/com/rpgbot/cs/discordbot/router/DbCommandRouter.java
@@ -3,6 +3,7 @@ package com.rpgbot.cs.discordbot.router;
 import com.rpgbot.cs.discordbot.annotations.Command;
 import com.rpgbot.cs.discordbot.events.CommandMessageEvent;
 import com.rpgbot.cs.discordbot.exception.CommandExistsException;
+import com.rpgbot.cs.discordbot.exception.CommandNotExistsException;
 import com.rpgbot.cs.discordbot.messagehandlers.DiscordMessage;
 import com.rpgbot.cs.discordbot.services.CommandService;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
@@ -31,17 +32,27 @@ public class DbCommandRouter {
 
     @Command(alias = "addcommand")
     public DiscordMessage addCommand(final CommandMessageEvent commandMessageEvent) {
-        if (commandMessageEvent.getCommand().equals("addcommand")) {
-            if (commandMessageEvent.getArgs().length >= 2) {
-                try {
-                    commandService.register(commandMessageEvent.getArgs()[0], String.join(" ", Arrays.copyOfRange(commandMessageEvent.getArgs(), 1, commandMessageEvent.getArgs().length)));
-                } catch (CommandExistsException commandExistsException) {
-                    return DiscordMessage.error(commandMessageEvent.getArgs()[0] + " already exists!");
-                }
-            } else {
-                return DiscordMessage.error("addcommand <command> <respond>", "adds a static command to the bot");
+        if (commandMessageEvent.getArgs().length >= 2) {
+            try {
+                commandService.register(commandMessageEvent.getArgs()[0], String.join(" ", Arrays.copyOfRange(commandMessageEvent.getArgs(), 1, commandMessageEvent.getArgs().length)));
+            } catch (CommandExistsException commandExistsException) {
+                return DiscordMessage.error(commandExistsException.getMessage());
             }
+        } else {
+            return DiscordMessage.error("addcommand <command> <respond>", "adds a static command to the bot");
         }
         return DiscordMessage.plain("command added");
+    }
+
+    @Command(alias = "modifycommand")
+    public DiscordMessage editCommand(final CommandMessageEvent commandMessageEvent) {
+        if (commandMessageEvent.getArgs().length >= 2) {
+            try {
+                commandService.modifyCommand(commandMessageEvent.getArgs()[0], String.join(" ", Arrays.copyOfRange(commandMessageEvent.getArgs(), 1, commandMessageEvent.getArgs().length)));
+            } catch (CommandNotExistsException commandNotExistsException) {
+                return DiscordMessage.error(commandNotExistsException.getMessage());
+            }
+        }
+        return DiscordMessage.plain("command modified");
     }
 }
